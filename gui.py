124da645e8e28d049b45d5d8b51d7be9ac3104db
Medonlakador Syiemlieh <medon.syiem@gmail.com>
2024-12-23 17:04:08 +0530

    GUI retouched

--- gui.py
+++ gui.py
@@ -14,22 +14,27 @@ def initialize():
                     [psg.Table({},
                            headings=["IP Address","Mac Address", "Vendor Name"],
                            key="VendorData",
-                           visible=False)]
+                           # visible=False,
+                           col_widths=[40,40,70])]
                 ]
-
+    psg.theme("Dark Grey 9")
     window = psg.Window('NetAnalyst',
                         layout,
-                        background_color="White",
                         size=(1920,1080),
                         finalize=True)
     window.maximize()
+
+    screen_width, screen_height = window.get_screen_dimensions()
+    window.move(0,0)
+    screen_width = screen_width/2
+    window.size=(screen_width, screen_height)
     
     while True:
         event, values = window.read()
+        window["VendorData"].update(visible = not window["VendorData"].visible)
         
         if event == 'Scan':
-            # window["Table123"].update(visible=not window["Table123"].visible)
-            window["VendorData"].update(visible = not window["VendorData"].visible)
+            window["VendorData"].update(visible = True)
             scan_result = scan.beginScan(values[0])
             new_table_data = [[row['ip'], row['mac'], row['vendorName']] for row in scan_result]
             window["VendorData"].update(values=new_table_data)
